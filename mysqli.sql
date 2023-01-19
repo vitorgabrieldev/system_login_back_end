60e593a1a559fb6cd252a3ea0e0a0982ec64cc01
Vitor Gabriel de Oliveira <106203763+vitorgabrieldev@users.noreply.github.com>
2023-01-19 20:01:07 -0300

    Update mysqli.sql

--- mysqli.sql
+++ mysqli.sql
@@ -3,7 +3,7 @@ CREATE DATABASE project_accounts_system;
 USE project_accounts_system;
 
 CREATE TABLE accounts (
-	idUser INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    idUser INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     username VARCHAR(140) NOT NULL,
     fullName VARCHAR(140) NOT NULL,
     age varchar(3) NOT NULL,
